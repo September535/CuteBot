8b6212e8d301dc267c0eaf02d4113dfa46af2361
September535 <ludejin_2003@163.com>
2025-02-18 23:20:00 +0800

    Update test.c

--- STC8H1K08/test.c
+++ STC8H1K08/test.c
@@ -46,7 +46,7 @@ bit isda; // 设备地址标志
 bit isma; // 存储地址标志
 unsigned char addr; // 存储接收到的内存地址
 unsigned char buffer[8]; // 存储接收到的数据
-int count_buffer,count,PWM;
+int count_buffer, count, PWM;
 
 // PWM占空比的控制变量 (0-255)
 unsigned char L_R_pwm = 0;  // 左侧红色亮度
@@ -69,10 +69,11 @@ void Delay1ms(void)	//@22.1184MHz
 	} while (--i);
 }
 
-void delay_ms(unsigned int ms){
-	unsigned int x ;
-  for(x = 0 ; x<ms ;++x){Delay1ms();}
-		
+void delay_ms(unsigned int ms) {
+	unsigned int x;
+    for (x = 0; x < ms; ++x) {
+        Delay1ms();
+    }
 }
 
 typedef struct {
@@ -107,6 +108,7 @@ void setRMotor(Motor *motor, int direction, int speed) {
         PWM_B2 = 0;
     }
 }
+
 // 控制M1电机的速度
 void setLMotor(Motor *motor, int direction, int speed) {
     if (speed < 0) speed = 0;
@@ -150,48 +152,25 @@ void updateMotors() {
     }
 }
 
-void Timer0_Init(void) {
-    TMOD = 0x02;  // 设置定时器0为自动重载模式
-    TH0 = 0xFF;   // 设置定时器初值
-    TL0 = 0xFF;
-    EA = 1;       // 开启总中断
-    ET0 = 1;      // 使能定时器0中断
-    TR0 = 1;      // 启动定时器0
-	
-	  PT0 = 0;      // Timer0 中断优先级为低
-}
+// 软件PWM控制RGB灯
+void Software_PWM_Control() {
+    static unsigned char pwm_counter = 0;
 
-// 定时器0中断服务函数，用于PWM输出
-void Timer0_ISR(void) interrupt 1 {
-    static unsigned int counter = 0;
-    counter++;
-    
-    if (counter < 255) {
-        // 左侧RGB控制
-        if (counter < L_R_pwm) L_RGB_r = 0;  // 红色亮度
-        else L_RGB_r = 1;
-        
-        if (counter < L_G_pwm) L_RGB_g = 0;  // 绿色亮度
-        else L_RGB_g = 1;
-        
-        if (counter < L_B_pwm) L_RGB_b = 0;  // 蓝色亮度
-        else L_RGB_b = 1;
+    // 控制左侧RGB灯
+    L_RGB_r = (pwm_counter < L_R_pwm) ? 0 : 1;
+    L_RGB_g = (pwm_counter < L_G_pwm) ? 0 : 1;
+    L_RGB_b = (pwm_counter < L_B_pwm) ? 0 : 1;
 
-        // 右侧RGB控制
-        if (counter < R_R_pwm) R_RGB_r = 0;  // 红色亮度
-        else R_RGB_r = 1;
-        
-        if (counter < R_G_pwm) R_RGB_g = 0;  // 绿色亮度
-        else R_RGB_g = 1;
-        
-        if (counter < R_B_pwm) R_RGB_b = 0;  // 蓝色亮度
-        else R_RGB_b = 1;
-    } else {
-        counter = 0;
+    // 控制右侧RGB灯
+    R_RGB_r = (pwm_counter < R_R_pwm) ? 0 : 1;
+    R_RGB_g = (pwm_counter < R_G_pwm) ? 0 : 1;
+    R_RGB_b = (pwm_counter < R_B_pwm) ? 0 : 1;
+
+    // 更新PWM计数器
+    pwm_counter++;
+    if (pwm_counter >= 255) {
+        pwm_counter = 0;
     }
-    
-    TH0 = 0xFF;   // 重载定时器
-    TL0 = 0xFF;
 }
 
 // 设置左侧RGB的亮度值
@@ -208,76 +187,55 @@ void Set_Right_RGB_Brightness(unsigned char red, unsigned char green, unsigned c
     R_B_pwm = blue;
 }
 
-void I2C_Isr() interrupt 24
-{
+void I2C_Isr() interrupt 24 {
     _push_(P_SW2);
     P_SW2 |= 0x80;
 
-    if (I2CSLST & 0x40)
-    {
-        I2CSLST &= ~0x40;                    //处理START事件
-    }
-    else if (I2CSLST & 0x20)
-    {
-        I2CSLST &= ~0x20;                       //处理RECV事件
-        if (isda)
-        {
-            isda = 0;                           //处理RECV事件（RECV DEVICE ADDR）
-        }
-        else if (isma)
-        {
-            isma = 0;                           //处理RECV事件（RECV MEMORY ADDR）
+    if (I2CSLST & 0x40) {
+        I2CSLST &= ~0x40; // 处理START事件
+    } else if (I2CSLST & 0x20) {
+        I2CSLST &= ~0x20; // 处理RECV事件
+        if (isda) {
+            isda = 0; // 处理RECV事件（RECV DEVICE ADDR）
+        } else if (isma) {
+            isma = 0; // 处理RECV事件（RECV MEMORY ADDR）
             addr = I2CRXD;
             I2CTXD = buffer[addr];
+        } else {
+            buffer[addr++] = I2CRXD; // 处理RECV事件（RECV DATA）
+            if (addr >= 8) addr = 0;
         }
-        else
-        {   
-            buffer[addr++] = I2CRXD;            //处理RECV事件（RECV DATA）
-					  if(addr>=8)addr=0;
-					 
+    } else if (I2CSLST & 0x10) {
+        I2CSLST &= ~0x10; // 处理SEND事件
+        if (I2CSLST & 0x02) {
+            I2CTXD = 0xff; // 接收到NAK则停止读取数据
+        } else {
+            I2CTXD = buffer[++addr]; // 接收到ACK则继续读取数据
         }
-    }
-    else if (I2CSLST & 0x10)
-    {
-        I2CSLST &= ~0x10;                       //处理SEND事件
-        if (I2CSLST & 0x02)
-        {
-            I2CTXD = 0xff;                      //接收到NAK则停止读取数据
-        }
-        else
-        {
-            I2CTXD = buffer[++addr];            //接收到ACK则继续读取数据
-        }
-    }
-    else if (I2CSLST & 0x08)
-    {
-        I2CSLST &= ~0x08;                       //处理STOP事件
+    } else if (I2CSLST & 0x08) {
+        I2CSLST &= ~0x08; // 处理STOP事件
         isda = 1;
         isma = 1;
     }
 
     _pop_(P_SW2);
 }
-  
-void main()
-{		
-//    unsigned char left_red, left_green, left_blue;
-//    unsigned char right_red, right_green, right_blue;
-	
-	// 初始化引脚为低电平
+
+void main() {
+    // 初始化引脚为低电平
     PWM_A1 = 0;
     PWM_A2 = 0;
     PWM_B1 = 0;
     PWM_B2 = 0;
-	
-	  R_RGB_r = 1; // 默认关闭RGB灯
+
+    R_RGB_r = 1; // 默认关闭RGB灯
     R_RGB_g = 1;
     R_RGB_b = 1;
-	
-	  L_RGB_r = 1; 
-  	L_RGB_g = 1;
-	  L_RGB_b = 1;
-	
+
+    L_RGB_r = 1;
+    L_RGB_g = 1;
+    L_RGB_b = 1;
+
     P0M0 = 0x00;
     P0M1 = 0xFF;
     P1M0 = 0x00;
@@ -293,43 +251,36 @@ void main()
 
     P_SW2 = 0x80;
 
-    I2CCFG = 0x81;                              //使能I2C从机模式
-    I2CSLADR = 0X30;                            //设置从机设备地址寄存器I2CSLADR=0101_1010B
-                                                //即I2CSLADR[7:1]=010_1101B,MA=0B。
-                                                //由于MA为0,主机发送的的设备地址必须与
-                                                //I2CSLADR[7:1]相同才能访问此I2C从机设备。
-                                                //主机若需要写数据则要发送5AH(0101_1010B)
-                                                //主机若需要读数据则要发送5BH(0101_1011B)
+    I2CCFG = 0x81; // 使能I2C从机模式
+    I2CSLADR = 0X30; // 设置从机设备地址
     I2CSLST = 0x00;
-    I2CSLCR = 0x78; // 0x00                     //禁止从机模式中断
-		PS = 1; 
-		EA = 1;
-		
-    isda = 1;                                   //用户变量初始化
+    I2CSLCR = 0x78; // 禁止从机模式中断
+    PS = 1;
+    EA = 1;
+
+    isda = 1; // 用户变量初始化
     isma = 1;
     addr = 1;
     I2CTXD = buffer[addr];
-		
-		initMotor(&leftMotor);
+
+    initMotor(&leftMotor);
     initMotor(&rightMotor);
-    
-		Timer0_Init();
+
+    while (1) {
 			
-    while (1){
-			
-        // 示例：左轮正转，速度为 70；右轮反转，速度为 50
-        setLMotor(&leftMotor, buffer[0], buffer[2]);  // 左轮正转，速度 70
+	setLMotor(&leftMotor, buffer[0], buffer[2]);  // 左轮正转，速度 70
         setRMotor(&rightMotor, buffer[1], buffer[3]); // 右轮反转，速度 50
 
         updateMotors(); // 更新电机状态
-        Delay1ms();    // 延时 1ms
+			
+        // 设置左侧RGB亮度
+        Set_Left_RGB_Brightness(119, 180, 119); // 红色最亮，绿色中等，蓝色较暗
 
-if(buffer[3] == 4 || buffer[3] == 6){
-	  Set_Left_RGB_Brightness(buffer[0], buffer[1], buffer[2]);
-	}
-	    
-if(buffer[3] == 5 || buffer[3] == 6){
-	  Set_Right_RGB_Brightness(buffer[0], buffer[1], buffer[2]);
-	}     
- }   
+        // 设置右侧RGB亮度
+        Set_Right_RGB_Brightness(180, 119, 135); // 红色较暗，绿色中等，蓝色最亮
+
+        // 更新RGB灯状态
+        Software_PWM_Control();
+
+    }
 }
